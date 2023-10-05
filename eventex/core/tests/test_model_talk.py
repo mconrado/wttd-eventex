f0f8a2773a0614101c05aded7426683639389b75
mconrado <mconrado@protonmail.com>
2023-10-05 14:45:15 -0300

    Course como MTI

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -43,6 +43,9 @@ class TalkModelTestCase(TestCase):
     def test_str(self):
         self.assertEqual('Título da Palestra', str(self.talk))
 
+    def test_ordering(self):
+        self.assertListEqual(['start'], Talk._meta.ordering)
+
 
 class TalkListGetEmpty(TestCase):
 
@@ -96,3 +99,6 @@ class CourseModelTest(TestCase):
 
     def test_manager(self):
         self.assertIsInstance(Course.objects, PeriodManager)
+
+    def test_ordering(self):
+        self.assertListEqual(['start'], Course._meta.ordering)
